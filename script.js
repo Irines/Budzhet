c0a0da2b8b67ae6e9c4506186258039129cdc6e5
Irine <irinesmith567@gmail.com>
2019-08-09 23:41:12 +0300

    script.js задание2 доработка + добавление циклов

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let money = prompt ("Ваш бюджет на месяц?")
-let time = prompt("Введите дату в формате YYYY-MM-DD")
-var a = prompt ("Введите обязательную статью расходов в этом месяце")
-var b = prompt("Во сколько обойдется?")
-var AppData = {
+let money = prompt ("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let AppData = {
     budzhet: money,
     timeData: time,
     savings: false,
     income: [],
     optionalExpenses: 0,
     expenses: {a:b}
-
+};
+for (let i=0; i<2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько обойдется?");
 }
-var oneDay = ((AppData.budzhet)/30).toFixed(1)
-alert(oneDay)
+var oneDay = ((AppData.budzhet)/30).toFixed(1);
+alert(oneDay);
 
 
